a729b4da24ffeddad1f5e5b5d5076e00415edb72
Thomas Dillard <Thomas.Dillard.studios@gmail.com>
2021-11-01 14:19:36 -0700

    Set config to new URL.
    Update `url` and `baseUrl` in `docusaurus.config.js` to match new url: https://devtionary.app

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://anthonyshew.github.io",
-  baseUrl: "/devtionary/",
+  url: "https://devtionary.app",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
