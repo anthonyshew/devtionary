b1b79e78cdf0e61f2bd75ec80ca70d3b0e4497a2
Anthony Shew <anthonyshew@gmail.com>
2021-11-07 10:37:58 -0700

    Bad path?

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: '/assets/meta-image.png',
+      image: '/img/meta-image.png',
       navbar: {
         title: "Devtionary",
         logo: {
